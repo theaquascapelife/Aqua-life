9bc5b2caa82fd22c9f35db7c6402fc82f6b4379b
Mohsanul786 <mdmohsan2407@gmail.com>
2026-05-09 20:05:23 +0530

    added some changes

--- src/lib/sanity.fetch.ts
+++ src/lib/sanity.fetch.ts
@@ -1,5 +1,11 @@
 import { sanityClient } from "./sanity.client";
 
 export async function fetchSanityData<T>(query: string) {
-  return sanityClient.fetch<T>(query);
+  return sanityClient.fetch<T>(
+    query,
+    {},
+    {
+      cache: "no-store",
+    },
+  );
 }
