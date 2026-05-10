dc66699d4f44497a8dee85a9a8f12ee01608cb26
Mohsanul786 <mdmohsan2407@gmail.com>
2026-05-10 10:59:46 +0530

    added some changes

--- src/utils/login-schema.ts
+++ src/utils/login-schema.ts
@@ -2,7 +2,7 @@ import {z} from "zod"
 
 export const loginSchema = z.object({
   email: z.string().email("Invalid email").trim(),
-  password: z.string().min(6, "Enter Valid Password"),
+  password: z.string().min(6, "Enter Valid "),
 });
 
 export type LoginData = z.infer<typeof loginSchema>;
